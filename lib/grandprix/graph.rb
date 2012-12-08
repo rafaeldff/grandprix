ce3969b1eac5c8bd8f71a1792c9bdd7c04e6b828
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-12-08 17:46:35 -0200

    Implementing the full-blown algorithm

--- lib/grandprix/graph.rb
+++ lib/grandprix/graph.rb
@@ -2,62 +2,59 @@ class Grandprix::Graph
   class Sort
     def initialize(graph)
       @graph = graph
-      @preds = {}
-      @succ  = {}
+      @preds_count = {}
+      @successors  = {}
     end
     
     def solve
       analize
 
-      #sort_graph
-      res = []
-      (0..10).each do |i|
-        ranks = @preds.select{|k,v| v == i }.keys
-        res = res + ranks
-      end
-      res
+      sort_graph
     end
 
     def analize
       init_tables
 
       @graph.each do |j, k|
-        @preds[k] += 1
-        @succ[j].push k
+        @preds_count[k] += 1
+        @successors[j].push k
       end
-
-      puts "preds: #{@preds.inspect}"
-      puts "succ: #{@succ.inspect}"
     end
 
     def init_tables
       @graph.each do |pair|
         j, k = pair
-        p j, k
-        @preds[j] = 0 
-        @preds[k] = 0
-        @succ[j] = []
-        @succ[k] = []
+        @preds_count[j] = 0 
+        @preds_count[k] = 0
+        @successors[j] = []
+        @successors[k] = []
       end
     end
 
     def sort_graph
-      def loop(out)
-        return if out.empty?
+      def loop(outs, left, res)
+        return res if outs.empty?
+        out, *new_outs = outs
 
+        successors = @successors[out]
+        successors.each do |suc|
+          @preds_count[suc] -= 1
+          new_outs.push suc if @preds_count[suc] == 0
+        end
+        loop new_outs, left-1, res.push(out)
       end
 
-      initial_out = indexes(@preds) {|x| x == 0 }
-      loop initial_out
+      initial_out = with_pred_count 0
+      loop initial_out, num_vertices, []
     end
 
     private
-    def indexes(hash)
-      res = []
-      hash.each do |x, i|
-        res.push i if yield(x)  
-      end
-      res
+    def with_pred_count n
+      @preds_count.select {|k,v| v == n }.keys
+    end
+
+    def num_vertices
+      @preds_count.size
     end
   end
 
--- spec/lib/grandprix/graph_spec.rb
+++ spec/lib/grandprix/graph_spec.rb
@@ -1,13 +1,46 @@
 require 'spec_helper'
 
+
 describe Grandprix::Graph do
   describe :topological_sort do
     it "should sort a linked list" do
-      subject.sort([[:a, :b],[:b, :c]]).should == [:a, :b, :c] 
+      edges = [[:a, :b],[:b, :c]]
+      subject.sort(edges).should be_an_ordering_of(edges)
+      subject.sort(edges).should == [:a, :b, :c] 
     end
     
     it "should sort a small graph" do
-      
+      #         d
+      #        ^^
+      #       /  \
+      # a -> b -> c <- e
+      edges = [
+        [:a, :b],
+        [:b, :c],
+        [:b, :d],
+        [:c, :d],
+        [:e, :c],
+      ]
+      subject.sort(edges).should  be_an_ordering_of(edges)
+    end
+
+    it "should sort a larger graph" do
+      # 
+      #               4 -> 6 <- 8  <-  2
+      #               ^         ^      ^
+      #               |         |      |
+      # 1  ->  3  ->  7   --->  5  <-  9
+      edges = [
+        [9, 2], [4, 6],
+        [3, 7], [1, 3],
+        [7, 5], [7, 4],
+        [5, 8], [9, 5],
+        [8, 6], [2, 8],
+      ]
+      res = subject.sort(edges)
+
+      #new_edges = edges.push [10, 20]
+      res.should  be_an_ordering_of(edges)
     end
   end  
 end
