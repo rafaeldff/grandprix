512da0f10d74279ebcf27613f6eeac54a8dfd1ce
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-12-14 17:59:54 -0200

    Formatting

--- spec/lib/grandprix/runner_spec.rb
+++ spec/lib/grandprix/runner_spec.rb
@@ -20,27 +20,27 @@ describe Grandprix::Runner do
     subject.run!(topology, elements).names.should == ["db", "frontend", "client"]
   end
 
-    it "a complex case" do
-      topology = {
-        "frontend" => {
-          "after" => ["backend", "assets"],    # frontend depends on assets in addxition to
-          "alongside" => ["assets", "images"]  # having it declared alongside
-        },
-        "backend" => {
-          "alongside" => ["external_backend"]
-        },
-        "images" => {
-          "after" => ["client"] #images that is an alongside dep of frontend depends on client
-        }
-      }
+  it "a complex case" do
+    topology = {
+      "frontend" => {
+      "after" => ["backend", "assets"],    # frontend depends on assets in addxition to
+      "alongside" => ["assets", "images"]  # having it declared alongside
+    },
+      "backend" => {
+      "alongside" => ["external_backend"]
+    },
+      "images" => {
+      "after" => ["client"] #images that is an alongside dep of frontend depends on client
+    }
+    }
 
-      elements = ["frontend", "client", "backend", "extra"] #extra is not mentioned on the topology
-      result = subject.run!(topology, elements)
+    elements = ["frontend", "client", "backend", "extra"] #extra is not mentioned on the topology
+    result = subject.run!(topology, elements)
 
-      result.names.should =~ ["client", "backend", "external_backend", "frontend", "assets", "images", "extra"]
-      result.should beOrderedHaving("backend", "assets").before("frontend")
-      result.should beOrderedHaving("backend", "assets").before("frontend", "assets", "images")
-      result.should beOrderedHaving("client").before("images")
-    end
-  
+    result.names.should =~ ["client", "backend", "external_backend", "frontend", "assets", "images", "extra"]
+    result.should beOrderedHaving("backend", "assets").before("frontend")
+    result.should beOrderedHaving("backend", "assets").before("frontend", "assets", "images")
+    result.should beOrderedHaving("client").before("images")
+  end
+
 end
